20aa1f017e2cf7bc76d959954e168c147e7bbf5a
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-16 21:29:11 -0300

    segundo commit

---
+++ web-api-restaurante/Controllers/ProdutoController.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using System.Data;
+using web_api_restaurante.Entidades;
+
+namespace web_api_restaurante.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProdutoController : ControllerBase
+    {
+        private readonly string? _connectionString;
+        //ctor atalho para criar o construtor
+        public ProdutoController(IConfiguration configuration) {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        private IDbConnection OpenConnection()
+        {
+            IDbConnection dbConnection = new SqliteConnection(_connectionString);
+            dbConnection.Open();
+            return dbConnection;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index() 
+        {
+            using IDbConnection dbConnection = OpenConnection();
+            string sql = "select id, nome, descricao, imageUrl from Produto; ";
+            var result = await dbConnection.QueryAsync<Produto>(sql);
+
+            return Ok(result);
+        }
+
+    }
+}
---
+++ web-api-restaurante/Entidades/Produto.cs
@@ -0,0 +1,13 @@
+﻿namespace web_api_restaurante.Entidades
+{
+    public class Produto {
+
+        //Atalho prop + tab
+
+        public int id { get; set; }
+        public string Nome { get; set;}
+        public string Descricao { get; set;}
+        public string ImageUrl { get; set;}
+
+    }
+}
